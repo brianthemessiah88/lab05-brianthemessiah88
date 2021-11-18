0aea93e6276d40c728415fe7b984ad1c373730c8
Giovanni Gasparini <giovannigasparini@ucsb.edu>
2021-11-17 19:20:13 -0800

    first round of commits

--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -3,5 +3,11 @@
 #include <iostream>
 
 int maxOfArray(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  int max=0;
+  for (int i = 0; i<size;i++){
+    if (a[i]>max){
+	    max = a[i];
+    }
+  }
+  return max;
 }
